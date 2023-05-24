f96a8e3a4308099301795383815dbad2a8a3b73d
talha073 <talhajavedd@gmail.com>
2023-05-24 22:13:13 +0400

    add events

--- contracts/multi-sig-wallet.sol
+++ contracts/multi-sig-wallet.sol
@@ -1,5 +1,10 @@
 // SPDX-License-Identifier: MIT
 contract multiSigWallet {
+    event Deposit(address indexed sender, uint amount);
+    event SubmitTransaction(uint indexed txIndex);
+    event Approve(address indexed owner, uint indexed txIndex);
+    event Revoke(address indexed owner, uint indexed txIndex);
+    event Execute(address indexed owner, uint indexed txIndex);
     
     
     constructor() {
