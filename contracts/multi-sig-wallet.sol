93eb1b4bd51f6ff0c71aaf0f9221b407b59c0d22
talha073 <talhajavedd@gmail.com>
2023-05-25 21:58:43 +0400

    complete contructor implimentation

--- contracts/multi-sig-wallet.sol
+++ contracts/multi-sig-wallet.sol
@@ -27,9 +27,10 @@ contract multiSigWallet {
         for(uint256 i = 0; i< _owners.length; i++){
             address owner = _owners[i];
             require(owner != address(0),  "invalid owner");
+            require(!isOwner[owner], "owner not unique");
+            isOwner[owner] = true;
+            owners.push(owner);
         }
-
-        
-        
+        required = _require;   
     }
 }
