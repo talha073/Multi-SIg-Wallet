f1fcaeb3a97409aa2208591564d98c92cf0eb0a8
talha073 <talhajavedd@gmail.com>
2023-05-27 23:05:49 +0400

    add deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,17 +1,17 @@
-// const hre = require("hardhat");
+const hre = require("hardhat");
 
-// async function main() {
-//   const MultiSigWallet = await hre.ethers.getContractFactory("multiSigWallet");
-//   [owner1, owner2, owner3, nonOwner] = await ethers.getSigners();
-//   const multiSigWallet = await MultiSigWallet.deploy(
-//     [owner1.address, owner2.address, owner3.address],
-//     2
-//   );
-//   await multiSigWallet.deployed();
-//   console.log("contract deployed at: ", multiSigWallet.address);
-// }
+async function main() {
+  const MultiSigWallet = await hre.ethers.getContractFactory("multiSigWallet");
+  [owner1, owner2, owner3, nonOwner] = await ethers.getSigners();
+  const multiSigWallet = await MultiSigWallet.deploy(
+    [owner1.address, owner2.address, owner3.address],
+    2
+  );
+  await multiSigWallet.deployed();
+  console.log("contract deployed at: ", multiSigWallet.address);
+}
 
-// main().catch((error) => {
-//   console.error(error);
-//   process.exitCode = 1;
-// });
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
